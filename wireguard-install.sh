8f5152d8077e0ebc7580579deea865358bb14599
Faisal Moledina <faisal.moledina@gmail.com>
2020-01-29 13:21:50 -0700

    Commented lines referring to adding unstable repo for debian, to use with debian system already on unstable

--- wireguard-install.sh
+++ wireguard-install.sh
@@ -88,8 +88,8 @@ if [[ "$OS" = 'ubuntu' ]]; then
     apt-get install "linux-headers-$(uname -r)"
     apt-get install wireguard iptables resolvconf
 elif [[ "$OS" = 'debian' ]]; then
-    echo "deb http://deb.debian.org/debian/ unstable main" > /etc/apt/sources.list.d/unstable.list
-    printf 'Package: *\nPin: release a=unstable\nPin-Priority: 90\n' > /etc/apt/preferences.d/limit-unstable
+    #echo "deb http://deb.debian.org/debian/ unstable main" > /etc/apt/sources.list.d/unstable.list
+    #printf 'Package: *\nPin: release a=unstable\nPin-Priority: 90\n' > /etc/apt/preferences.d/limit-unstable
     apt update
     apt-get install "linux-headers-$(uname -r)"
     apt install wireguard iptables resolvconf
